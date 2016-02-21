0209130068e028b4acebb779ae2d1b59dda97413
Rice <wendykjhgfdsa@gmail.com>
2016-02-21 18:09:07 +0800

    cc
    cc

--- js/load.js
+++ js/load.js
@@ -18,17 +18,17 @@ $('.wrapper').show();
 
 //loaderSpinner();
 
-/*
+
 $(window).scroll(function() {
 
     if ($(window).scrollTop() > 100) {
-        $( '.nav-toggle' ).css( "background-color", "rgba(255, 255, 255, 0)" );
+        $( '.nav-toggle' ).css( "background-color", "rgba(46, 46, 46, 1)" );
     } else {
-        $( '.nav-toggle' ).css( "background-color", "rgba(255, 255, 255, 1)" );
+        $( '.nav-toggle' ).css( "background-color", "rgba(255, 255, 255, 0)" );
     }
 });
 
-*/
+
 
 
 $('.nav-toggle').click(function(){
