b2cb9b1a2a935bbed5591c93a6d105b442c41897
Rice <wendykjhgfdsa@gmail.com>
2016-02-28 16:18:55 +0800

    cc
    cc

--- dist/js/jquery.lazyload.js
+++ dist/js/jquery.lazyload.js
@@ -29,7 +29,8 @@
             skip_invisible  : false,
             appear          : null,
             load            : null,
-            placeholder     : "data:image/"
+            //placeholder     : "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXYzh8+PB/AAffA0nNPuCLAAAAAElFTkSuQmCC"
+            placeholder     : "background-color:#F8F8F8;"
         };
 
         function update() {
